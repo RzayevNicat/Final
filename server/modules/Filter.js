d726f0da8d1e34c8a4dafc586965e0b2f8a5e281
Nicat Rzayev <99348762+RzayevNicat@users.noreply.github.com>
2023-03-01 03:31:16 +0400

    Delete server directory

--- server/modules/Filter.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const { Schema } = mongoose;
-const filterSchema = new Schema({
-	color: { type: Array, required: true },
-	size: { type: Array, required: true }
-});
-
-module.exports = mongoose.model('Filter', filterSchema);
