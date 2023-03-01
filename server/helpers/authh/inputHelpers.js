9ed2d5b8d05266d60aab6a1a5a6284a40690a902
Nicat Rzayev <99348762+RzayevNicat@users.noreply.github.com>
2023-03-01 04:03:52 +0400

    Delete server directory

--- server/helpers/authh/inputHelpers.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcryptjs');
-const validateInputs = (email, password) => {
-	return email && password;
-};
-const comparePassword = (password, hashedPassword) => {
-	return bcrypt.compareSync(password, hashedPassword);
-};
-module.exports = { validateInputs, comparePassword };
--- server/routers/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllUser, getUser, deleteUser, updateUser } = require('../controllers/users');
-router.get('/users', getAllUser);
-router.get('/users/:id', getUser);
-router.delete('/users/:id', deleteUser);
-router.put('/users/:id', updateUser);
-
-module.exports = router;
